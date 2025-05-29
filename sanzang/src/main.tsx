d3d208cc56264546053b7315e1c638462c50882b
connortbot <loiconnor8@gmail.com>
2025-05-29 10:19:14 -0700

    styling

--- sanzang/src/main.tsx
+++ sanzang/src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./Home";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
